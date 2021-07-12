319c4ade35723b6e7996f14fc3921b7423d82f38
Hippo Spark (Arch) <hippospark@gmail.com>
2021-07-12 11:52:39 +0800

    更新了依赖包 jslogiccircuit 的版本。

--- src/abstractportitem.js
+++ src/abstractportitem.js
@@ -8,7 +8,7 @@ class AbstractPortItem {
     }
 
     isValid() {
-        // 端口名称名称只可以包含 [0-9a-zA-Z_\$] 字符，且只能以 [a-zA-Z_] 字符开头
+        // 端口名称只可以包含 [0-9a-zA-Z_\$] 字符，且只能以 [a-zA-Z_] 字符开头
         return /^[a-zA-Z_][\w\$]*(\.[a-zA-Z_][\w\$]*)*$/.test(this.namePath);
     }
 }
